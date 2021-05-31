7b45b685e6952157a27ffe8f217acaee73db9c6d
EilDomas <82733895+EilDomas@users.noreply.github.com>
2021-06-01 01:14:29 +0300

    made first homework

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,27 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello please write down your name:");
+            var name = Console.ReadLine();
+            //Console.WriteLine(name);
+            Console.WriteLine("Hello please write down your surname:");
+            var surename = Console.ReadLine();
+            //Console.WriteLine(surename);
+            Console.WriteLine("Hello please write down your age:");
+            var age = Console.ReadLine();
+            //Console.WriteLine(age);
+            Console.WriteLine("Hello please write down your weight (kg):");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(weight);
+            Console.WriteLine("Hello please write down your height (cm):");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(height);
+            double heightCalc = height / 100;
+            double heightSquare = heightCalc * heightCalc;
+            Console.WriteLine(heightSquare);
+            double bmi = weight / heightSquare;
+            bmi = Math.Round(bmi, 1);
+            Console.WriteLine(bmi);
         }
     }
 }
